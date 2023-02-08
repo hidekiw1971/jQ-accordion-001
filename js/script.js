0d73d2f3ccdcb3a362773c42b791b47abf9fe897
hideki watabe <hideki.w.1971@gmail.com>
2023-02-09 06:31:01 +0900

    jQuery 横開きアコーディオン完成（responsive対応）

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+
+  // 横アコーディオン
   $(function () {
     // #acc1以外を縮める
     $('dd[id != "acc1"]').css("width", "0px");
@@ -9,13 +11,12 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
   $("a").click(function () {
     $(".current").animate({ "width": "0px" }, 0);
     // click時処理
-    $($(this).attr("href")).animate({ "width": "800px" }, 0);
+    $($(this).attr("href")).animate({ "width": "80%" }, 0);
     $("dd").removeClass();
     $($(this).attr("href")).addClass("current");
     $("a").removeClass();
     $(this).addClass("currentBtn");
     return false;
   });
-
   // ↓消さない
 });
